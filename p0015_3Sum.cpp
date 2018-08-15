251b151b92e441e09b58f01652196751de454057
StarlingNiohuru <sider.niohuru@gmail.com>
2018-08-15 16:13:32 +0800

    p0015 finished

--- p0015_3Sum.cpp
+++ p0015_3Sum.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <iostream>
-#include <unordered_set>
 #include <iterator>
 #include <algorithm>
 
@@ -9,29 +8,23 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int> &nums) {
+        if (nums.empty()) { return {}; }
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        unordered_set<int> set;
-        for (auto it1 = nums.begin(); it1 != nums.end(); ++it1) {
-            for (auto it2 = it1 + 1; *it2 <= 0 - *it1; ++it2) {
-                auto got = set.find(0 - *it1 - *it2);
-                if (got == set.end()) {
-                    set.insert(*it2);
-                } else {
-                    res.push_back({*it1, *it2, 0 - *it1 - *it2});
-                    cout << *it1 << "," << *it2 << "," << 0 - *it1 - *it2 << endl;
+        for (auto it0 = nums.begin(); *it0 <= 0; ++it0) {
+            if (it0 > nums.begin() && *it0 == *(it0 - 1)) { continue; }
+            int target = 0 - *it0;
+            auto itf = it0 + 1, ite = nums.end() - 1;
+            while (itf < ite) {
+                if (*itf + *ite < target || (itf > it0 + 1 && *itf == *(itf - 1))) { ++itf; }
+                else if (*itf + *ite > target || (ite < nums.end() - 1 && *ite == *(ite + 1))) { --ite; }
+                else {
+                    res.push_back({*it0, *itf, *ite});
+                    ++itf;
+                    --ite;
                 }
             }
         }
         return res;
     }
 };
-
-int main() {
-    Solution solution;
-    vector<int> nums = {-1, 0, 1, 2, -1, -4};
-    int target = 9;
-    vector<vector<int>> result = solution.threeSum(nums);
-//    cout << result[0] << "," << result[1] << endl;
-    return 0;
-}
