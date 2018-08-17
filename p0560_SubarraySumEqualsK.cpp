f7a2be69cbe57b76df680b8a8ff7b1dd5cfb771a
Starling <Sider@qq.com>
2018-08-18 00:32:39 +0800

    p0560 finished v2

--- p0560_SubarraySumEqualsK.cpp
+++ p0560_SubarraySumEqualsK.cpp
@@ -9,11 +9,12 @@ public:
     int subarraySum(vector<int> &nums, int k) {
         int res = 0;
         int sum = 0;
-        unordered_map<int, int> map = {{0, 1}};
+        unordered_map<int, int> map;
+        ++map[0];
         for (auto n:nums) {
             sum += n;
-            if (map.find(sum - k) != map.end()) { res += map[sum - k]; }
-            map[sum] += 1;
+            res += map[sum - k];
+            ++map[sum];
         }
         return res;
     }
