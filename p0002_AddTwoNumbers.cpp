177628a323fca1fc65bdb633905f712f277fb98f
Starling <Sider@qq.com>
2018-08-19 00:03:01 +0800

    p0002 finished

---
+++ p0002_AddTwoNumbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode res(0), *p = &res;
+        int carry = 0;
+        int sum;
+        while (l1 || l2 || carry) {
+            sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+        return res.next;
+    }
+};
