7773d5434e468876120ad5aa0b4dfe35597fa64c
StarlingNiohuru <sider.niohuru@gmail.com>
2018-09-04 18:20:28 +0800

    p0077 finished

---
+++ p0077_Combinations.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res, tmp;
+        int i = 0;
+        vector<int> s(k, 0);
+        s[0] = 1;
+        while (i >= 0) {
+            if (i < k - 1) {
+                if (s[i] < n) {
+                    s[i + 1] = s[i] + 1;
+                    i++;
+                } else {
+                    i--;
+                    if (i >= 0) { s[i]++; };
+                }
+            } else {
+                res.push_back(s);
+                if (s[i] == n) { i--; }
+                if (i >= 0) { s[i]++; };
+            }
+        }
+        return res;
+    }
+};
