8593f5fb045a449ced68c296d3285585bf64c74b
Starling <Sider@qq.com>
2019-09-09 23:21:05 +0800

    p0013 finished

---
+++ p0013_RomantoInteger.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        unordered_map<char, int> map = {{'I', 1},
+                                        {'V', 5},
+                                        {'X', 10},
+                                        {'L', 50},
+                                        {'C', 100},
+                                        {'D', 500},
+                                        {'M', 1000}};
+        for (int i = 0; i < s.size() - 1; i++) {
+            (map[s[i]] >= map[s[i + 1]]) ? (res += map[s[i]]) : (res -= map[s[i]]);
+        }
+        res += map[s.back()];
+        return res;
+    }
+};
