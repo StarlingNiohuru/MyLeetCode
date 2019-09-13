6f8d4e5c5ddf718c1681a90ede48d12a20cc7075
Starling <Sider@qq.com>
2019-09-13 12:26:24 +0800

    p0017 finished v2

--- p0017_LetterCombinationsofaPhoneNumber.cpp
+++ p0017_LetterCombinationsofaPhoneNumber.cpp
@@ -3,7 +3,36 @@
 
 using namespace std;
 
+//better and clear iteration like bfs
 class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())return {};
+        vector<string> res;
+        unordered_map<char, string> map = {{'2', "abc"},
+                                           {'3', "def"},
+                                           {'4', "ghi"},
+                                           {'5', "jkl"},
+                                           {'6', "mno"},
+                                           {'7', "pqrs"},
+                                           {'8', "tuv"},
+                                           {'9', "wxyz"}};
+        res.push_back("");
+//        res1 is the result of digits[0:-2],use res1 to generate res
+        for (int i = 0; i < digits.size(); i++) {
+            vector<string> res1;
+            string chars = map[digits[i]];
+            for (int j = 0; j < res.size(); j++)
+                for (int k = 0; k < chars.size(); k++)
+                    res1.push_back(res[j] + chars[k]);
+            res = res1;
+        }
+        return res;
+    }
+};
+
+//poor dfs iteration
+class Solution2 {
 public:
     vector<string> letterCombinations(string digits) {
         if (digits.empty())return {};
