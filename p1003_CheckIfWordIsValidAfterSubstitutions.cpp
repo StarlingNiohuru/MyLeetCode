f3351a3e7b7e661bdfc037e1c384bbb44e0de89a
Starling <Sider@qq.com>
2019-10-10 22:01:06 +0800

    p1003 finished v2

--- p1003_CheckIfWordIsValidAfterSubstitutions.cpp
+++ p1003_CheckIfWordIsValidAfterSubstitutions.cpp
@@ -1,9 +1,29 @@
 #include <string>
+#include <stack>
 
 using namespace std;
 
-//find and remove "abc" until there is no "abc"
+//use a stack to replace "abc". performance is much better
 class Solution {
+public:
+    bool isValid(string S) {
+        stack<char> s;
+        for (char c:S) {
+            if (c == 'c') {
+                if (s.size() >= 2 && s.top() == 'b') {
+                    s.pop();
+                    if (s.top() == 'a') s.pop();
+                    else return false;
+                } else return false;
+
+            } else s.push(c);
+        }
+        return s.empty();
+    }
+};
+
+//find and remove "abc" until there is no "abc"
+class Solution2 {
 public:
     bool isValid(string S) {
         while (!S.empty()) {
