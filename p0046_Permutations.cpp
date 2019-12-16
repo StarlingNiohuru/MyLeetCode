368e2a1afa13d3f41749a783f4297cb36c682917
Starling <Sider@qq.com>
2019-12-16 18:06:42 +0800

    p0046 finished

---
+++ p0046_Permutations.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+// Backtracking. Swap the current num with every num behind to create all the permutations.
+class Solution {
+private:
+    void backtrack(vector<int> &nums, vector<vector<int>> &res, int i) {
+        if (i == nums.size()) res.push_back(nums);
+        for (int j = i; j < nums.size(); j++) {
+            swap(nums[i], nums[j]);
+            backtrack(nums, res, i + 1);
+            swap(nums[i], nums[j]);
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int> &nums) {
+        vector<vector<int>> res;
+        backtrack(nums, res, 0);
+        return res;
+    }
+};
