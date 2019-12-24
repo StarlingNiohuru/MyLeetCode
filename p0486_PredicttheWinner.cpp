27b951fe300b2a68aabec7d5dffb0c4cbfbe19db
Starling <Sider@qq.com>
2019-12-24 19:07:06 +0800

    p0486 finished, perhaps need to improve

---
+++ p0486_PredicttheWinner.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+// dp iteration. dp[i][j] represents the max score the first player can gain from nums[i,j]. The equation is:
+// dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]) when i!=j. Time complexity is O(n2).
+class Solution {
+public:
+    bool PredictTheWinner(vector<int> &nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) dp[i][j] = nums[i];
+                else dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] >= 0;
+    }
+};
