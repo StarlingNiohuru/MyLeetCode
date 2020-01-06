794b89e9c279200073e6d784c4031338cbe0ced8
Starling <Sider@qq.com>
2020-01-06 22:03:34 +0800

    p0058 finished

---
+++ p0058_LengthofLastWord.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+// Time complexity is O(n).
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ') i--;
+        int j = i;
+        while (j >= 0 && s[j] != ' ') j--;
+        return i - j;
+    }
+};
+
