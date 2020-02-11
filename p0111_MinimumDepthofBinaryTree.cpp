a791674e389f462025dace713d29e35ce9509793
Starling <Sider@qq.com>
2020-02-12 00:46:49 +0800

    p0111 finished

---
+++ p0111_MinimumDepthofBinaryTree.cpp
@@ -0,0 +1,35 @@
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// similar to p0102. BFS iteration. Time complexity is O(n), space complexity is O(n).
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        int res = 0;
+        if (!root) return res;
+        queue<TreeNode *> q;
+        int count;
+        q.push(root);
+        while (!q.empty()) {
+            res++;
+            count = q.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode *curr = q.front();
+                q.pop();
+                if (curr->left) q.push(curr->left);
+                if (curr->right) q.push(curr->right);
+                if (!curr->left && !curr->right) return res;
+            }
+        }
+        return res;
+    }
+};
