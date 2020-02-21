1d37fea75a44983fa599459380a45ea4bdc8d709
Starling <Sider@qq.com>
2020-02-22 00:40:48 +0800

    p0224 comment & variable name fixed

--- p0224_BasicCalculator.cpp
+++ p0224_BasicCalculator.cpp
@@ -3,46 +3,46 @@
 
 using namespace std;
 
-// Use 2 stacks. One is used to record operands while another is for numbers. When meet '+','-' or '(' push former
-// number and operand into each stack. When meet ')', pop and calculate from 2 stacks until meet '(' then push the sum
-// into stack. At last pop and calculate all from 2 stacks. Time complexity is O(n) and space complexity is O(n).
+// Use 2 stacks. One is used to record operators while another is for numbers. When meet '+','-' or '(' push former
+// numbers and operators into each stack. When meet ')', pop and calculate from 2 stacks until meet '(' then push the
+// sum into stack. At last pop and calculate all from 2 stacks. Time complexity is O(n) and space complexity is O(n).
 class Solution {
 public:
     int calculate(string s) {
-        stack<char> operand;
-        stack<int> number;
+        stack<char> operators;
+        stack<int> numbers;
         s += '#';
         int res = 0, temp = 0, last;
         for (char c:s) {
             if (isdigit(c)) {
-                if (operand.empty() || operand.top() == '(') operand.push('+');
+                if (operators.empty() || operators.top() == '(') operators.push('+');
                 temp = temp * 10 + (c - '0');
             } else if (!isspace(c)) {
                 if (c == ')') {
                     last = 0;
-                    number.push(temp);
-                    while (operand.top() != '(') {
-                        if (operand.top() == '+') last += number.top();
-                        else last -= number.top();
-                        operand.pop();
-                        number.pop();
+                    numbers.push(temp);
+                    while (operators.top() != '(') {
+                        if (operators.top() == '+') last += numbers.top();
+                        else last -= numbers.top();
+                        operators.pop();
+                        numbers.pop();
                     }
-                    operand.pop();
+                    operators.pop();
                     temp = last;
                 } else {
-                    if (operand.empty() || operand.top() == '(') operand.push('+');
-                    operand.push(c);
-                    if (c != '(') number.push(temp);
+                    if (operators.empty() || operators.top() == '(') operators.push('+');
+                    operators.push(c);
+                    if (c != '(') numbers.push(temp);
                     temp = 0;
                 }
             }
         }
-        operand.pop();
-        while (!number.empty()) {
-            if (operand.top() == '+') res += number.top();
-            else res -= number.top();
-            operand.pop();
-            number.pop();
+        operators.pop();
+        while (!numbers.empty()) {
+            if (operators.top() == '+') res += numbers.top();
+            else res -= numbers.top();
+            operators.pop();
+            numbers.pop();
         }
         return res;
     }
