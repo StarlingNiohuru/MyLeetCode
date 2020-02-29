df411fab656a17c9d857f4af39f313e4d7c461e9
Starling <Sider@qq.com>
2020-03-01 01:14:54 +0800

    p0145 finished v3

--- p0145_BinaryTreePostorderTraversal.cpp
+++ p0145_BinaryTreePostorderTraversal.cpp
@@ -65,3 +65,21 @@ public:
         return res;
     }
 };
+
+// dfs recursion.
+class Solution3 {
+private:
+    void postorder(vector<int> &res, TreeNode *root) {
+        if (!root) return;
+        postorder(res, root->left);
+        postorder(res, root->right);
+        res.push_back(root->val);
+    }
+
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> res;
+        postorder(res, root);
+        return res;
+    }
+};
