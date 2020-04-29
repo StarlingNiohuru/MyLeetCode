f4d7102df212996d253ee412665ee9e7eb06a183
Starling <Sider@qq.com>
2020-04-29 15:40:31 +0800

    p0836 finished

---
+++ p0836_RectangleOverlap.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+// Enumerate 4 cases and find the common solution.
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int> &rec1, vector<int> &rec2) {
+        int x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3], x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
+        return (x1 < x4 && x3 < x2 && y1 < y4 && y3 < y2);
+    }
+};
