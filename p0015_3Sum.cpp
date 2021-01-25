d3f8cc741b3e84a7bb931a1b63f457609baca038
StarlingNiohuru <sider.niohuru@gmail.com>
2021-01-25 00:20:22 +0000

    p0015 finished, simple version with comment

--- p0015_3Sum.cpp
+++ p0015_3Sum.cpp
@@ -1,27 +1,26 @@
 #include <vector>
-#include <iostream>
-#include <iterator>
 #include <algorithm>
 
 using namespace std;
 
+// Use 2 pointers in a sorted list to find a pair(a,b) satisfying b+c=-a.
+// Time complexity is O(n2), space complexity is O(1).
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int> &nums) {
         if (nums.empty()) { return {}; }
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        for (auto it0 = nums.begin(); *it0 <= 0; ++it0) {
-            if (it0 > nums.begin() && *it0 == *(it0 - 1)) { continue; }
-            int target = 0 - *it0;
-            auto itf = it0 + 1, ite = nums.end() - 1;
-            while (itf < ite) {
-                if (*itf + *ite < target || (itf > it0 + 1 && *itf == *(itf - 1))) { ++itf; }
-                else if (*itf + *ite > target || (ite < nums.end() - 1 && *ite == *(ite + 1))) { --ite; }
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] < 0 || (l > i + 1 && nums[l] == nums[l - 1])) l++;
+                else if (nums[i] + nums[l] + nums[r] > 0 || (r < nums.size() - 1 && nums[r] == nums[r + 1])) r--;
                 else {
-                    res.push_back({*it0, *itf, *ite});
-                    ++itf;
-                    --ite;
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
                 }
             }
         }
