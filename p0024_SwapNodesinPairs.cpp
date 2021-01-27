f0768488d58dd0572c8145663a0d5af2e7d8d913
StarlingNiohuru <sider.niohuru@gmail.com>
2021-01-27 23:08:29 +0000

    p0024 finished v2, simple version with comment

--- p0024_SwapNodesinPairs.cpp
+++ p0024_SwapNodesinPairs.cpp
@@ -7,7 +7,25 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+//Iteration. 3 pointers in 4 steps.Time complexity is O(n), space complexity is O(1).
 class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        ListNode *dummy = new ListNode(0), *p = dummy;
+        dummy->next = head;
+        while (p->next && p->next->next) {
+            ListNode *p1 = p->next, *p2 = p->next->next;
+            p->next = p2;
+            p1->next = p2->next;
+            p2->next = p1;
+            p = p->next->next;
+        }
+        return dummy->next;
+    };
+};
+
+//Iteration. 3 pointer2 in 5 steps.
+class Solution2 {
 public:
     ListNode *swapPairs(ListNode *head) {
         ListNode *preHead = new ListNode(0);
